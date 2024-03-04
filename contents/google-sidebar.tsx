8219e08be249935ea96e3ab7e2b7c4d68b1c692b
Ajitesh13 <aji.yash13@gmail.com>
2024-03-04 11:23:00 +0530

    modal files added

--- contents/google-sidebar.tsx
+++ contents/google-sidebar.tsx
@@ -1,7 +1,19 @@
-import iconBase64 from "data-base64:~assets/icon.png";
-import cssText from "data-text:~/contents/google-sidebar.css";
 import type { PlasmoCSConfig } from "plasmo";
 import { useEffect, useState } from "react";
+import {
+  ChakraBaseProvider,
+  extendBaseTheme,
+  theme as chakraTheme,
+} from "@chakra-ui/react";
+const { Button } = chakraTheme.components;
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+});
+
+import iconBase64 from "data-base64:~assets/icon.png";
+import cssText from "data-text:~/contents/google-sidebar.css";
 
 // Inject to the webpage itself
 import "./google-sidebar-base.css";
@@ -22,42 +34,67 @@ export const getShadowHostId = () => "plasmo-google-sidebar";
 const GoogleSidebar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [openModal, setOpenModal] = useState<boolean>(false);
+  const [url, setUrl] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
 
   useEffect(() => {
+    setTitle(document.title);
+    setUrl(window.location.href);
     document.body.classList.toggle("plasmo-google-sidebar-show", isOpen);
   }, [isOpen]);
 
   return (
-    <div id="sidebar" className={isOpen ? "open" : "closed"}>
-      <div className="floating-container">
-        <button className="sidebar-toggle" onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? "🟡 Close" : "🟣 Open"}
-        </button>
-        <button
-          className="float-button"
-          onClick={() => setOpenModal(!openModal)}
-        >
-          Open Modal
-        </button>
-      </div>
-      <img src={iconBase64} alt="Extension Icon" width={128} height={128} />
-      <p>Your second brain is getting ready to chat</p>
-      {openModal && (
-        <div className="modal-overlay">
-          <div className="modal">
-            <div className="modal-content">
-              <button
-                className="modal-close-btn"
-                onClick={() => setOpenModal(false)}
-              >
-                Close
-              </button>
-              Hello There! Whant do you want to save now ?
+    <ChakraBaseProvider theme={theme}>
+      <div id="sidebar" className={isOpen ? "open" : "closed"}>
+        <div className="floating-container">
+          <button className="sidebar-toggle" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? "🟡 Close" : "🟣 Open"}
+          </button>
+          <button
+            className="float-button"
+            onClick={() => setOpenModal(!openModal)}
+          >
+            Open Modal
+          </button>
+        </div>
+        <img src={iconBase64} alt="Extension Icon" width={128} height={128} />
+        <p>Your second brain is getting ready to chat</p>
+        {openModal && (
+          <div className="modal-overlay">
+            <div className="modal">
+              <div className="modal-content">
+                <button
+                  className="modal-close-btn"
+                  onClick={() => setOpenModal(false)}
+                >
+                  X
+                </button>
+                Hello There! Whant do you want to save now ?
+                <div className="input-field">
+                  <div>
+                    <label>Bookmark title</label>
+                    <input
+                      type="text"
+                      value={title}
+                      style={{ width: "300px" }}
+                    />
+                  </div>
+                  <div>
+                    <label>Bookmark URL</label>
+                    <input type="text" value={url} style={{ width: "300px" }} />
+                  </div>
+                  <div>
+                    <label>Folder name</label>
+                    <input type="text" style={{ width: "300px" }} />
+                  </div>
+                  <button onClick={() => console.log("Saved")}>Save!</button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </ChakraBaseProvider>
   );
 };
 
