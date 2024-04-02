474003db36fa3fc3abac095adf485709a035fb96
Ajitesh13 <aji.yash13@gmail.com>
2024-04-02 18:50:13 +0530

    backend server fixed

--- src/util/Api/chat.ts
+++ src/util/Api/chat.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const backendUrl = "http://127.0.0.1:5000";
+export const backendUrl = process.env.PLASMO_PUBLIC_BACKEND_URL;
 
 export const chat = async ({
   userId,
